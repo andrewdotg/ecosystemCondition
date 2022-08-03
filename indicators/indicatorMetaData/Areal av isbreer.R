e643632ecc1341069e18d49f8783043c9da4fa3f
Anders Kolstad <anders.kolstad@nina.no>
2022-08-03 11:38:24 +0200

    Faktaark

---
+++ indicators/indicatorMetaData/Areal av isbreer.R
@@ -0,0 +1,33 @@
+# Areal av isbreer ----
+
+
+# indikatorens navn ----
+indikator <- "Areal av isbreer"
+
+# Utfylling av protokollen ----
+utfylling <- "Anders L. Kolstad"
+
+# Dato utfylt/revidert -----
+dato <- "08.12.2021"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#-------------------------------
+
+# Sett sammen til dataframe ----
+out <- data.frame(
+  "Indikator" = indikator,
+  "Utfylling av protokollen" = utfylling,
+  "Data utfylt/revidert" = dato
+)
