8ac65b7b28cf4cf96dba41d1a455e9c9d22ee47e
joatop <joachim.topper@nina.no>
2022-12-22 09:08:43 +0100

    upload and handling ANO data

--- R/functionalPlantIndicators/data handling.R
+++ R/functionalPlantIndicators/data handling.R
@@ -8,12 +8,16 @@ library(tidyverse)
 
 # url <- "https://nedlasting.miljodirektoratet.no/naturovervaking/naturovervaking_eksport.gdb.zip"
 
-# download from P-drive
+#### download data from P-drive ####
 
 # ANO
 st_layers(dsn = "P:/41201785_okologisk_tilstand_2022_2023/data/Naturovervaking_eksport.gdb")
-ANO.dat <- st_read("P:/41201785_okologisk_tilstand_2022_2023/data/Naturovervaking_eksport.gdb")
-head(ANO.dat)
+ANO.sp <- st_read("P:/41201785_okologisk_tilstand_2022_2023/data/Naturovervaking_eksport.gdb",
+                   layer="ANO_Art")
+ANO.geo <- st_read("P:/41201785_okologisk_tilstand_2022_2023/data/Naturovervaking_eksport.gdb",
+                  layer="ANO_SurveyPoint")
+head(ANO.sp)
+head(ANO.geo)
 
 # Tyler indicator data
 ind.Tyler <- read.table("P:/41201785_okologisk_tilstand_2022_2023/data/functional plant indicators/Tyler et al_Swedish plant indicators.txt",
@@ -28,6 +32,7 @@ head(ind.Grime)
 load("P:/41201785_okologisk_tilstand_2022_2023/data/functional plant indicators//reference from NiN/Eco_State.RData")
 str(Eco_State)
 
+#### data handling - functional indicator data ####
 # trimming away sub-species & co, and descriptor info
 ind.Grime[,'species.orig'] <- ind.Grime[,'species']
 ind.Grime[,'species'] <- word(ind.Grime[,'species'], 1,2)
@@ -118,3 +123,47 @@ ind.dat$species <- as.factor(ind.dat$species)
 summary(ind.dat$species)
 head(ind.dat)
 
+#### data handling - ANO data ####
+head(ANO.sp)
+head(ANO.geo)
+
+# fix species names
+ANO.sp$Species <- ANO.sp$art_navn
+unique(as.factor(ANO.sp$Species))
+#ANO.sp$Species <- sub(".*?_", "", ANO.sp$Species) # lose the Norwegian name in the front
+ANO.sp[,'Species'] <- word(ANO.sp[,'Species'], 1,2) # lose subspecies
+ANO.sp$Species <- str_to_title(ANO.sp$Species) # make first letter capital
+#ANO.sp$Species <- gsub("_", " ", ANO.sp$Species) # replace underscore with space
+ANO.sp$Species <- gsub("( .*)","\\L\\1",ANO.sp$Species,perl=TRUE) # make capital letters after hyphon to lowercase
+ANO.sp$Species <- gsub("( .*)","\\L\\1",ANO.sp$Species,perl=TRUE) # make capital letters after space to lowercase
+
+## merge species data with indicators
+ANO.sp <- merge(x=ANO.sp[,c("Species", "art_dekning", "ParentGlobalID")], 
+                y= ind.dat[,c("species","CC", "SS", "RR", "Light", "Moisture", "Soil_reaction_pH", "Nitrogen", "Grazing_mowing")],
+                by.x="Species", by.y="species", all.x=T)
+summary(ANO.sp)
+
+## adding information on ecosystem and condition variables
+ANO.sp <- merge(x=ANO.sp, 
+                y=ANO.geo[,c("GlobalID","ano_flate_id","ano_punkt_id","ssb_id","aar",
+                             "hovedoekosystem_punkt","hovedtype_1m2","kartleggingsenhet_1m2",
+                             "vedplanter_total_dekning","busker_dekning","tresjikt_dekning","roesslyng_dekning")], 
+            by.x="ParentGlobalID", by.y="GlobalID", all.x=T)
+# trimming away the points without information on NiN, species or cover
+ANO.sp <- ANO.sp[!is.na(ANO.sp$Species),]
+ANO.sp <- ANO.sp[!is.na(ANO.sp$art_dekning),]
+
+
+
+
+
+
+#ANO.sp <- ANO.sp[!is.na(ANO.sp$Hovedoekosystem_rute),] # need to check
+unique(as.factor(ANO.sp$hovedoekosystem_punkt))
+unique(as.factor(ANO.sp$hovedtype_1m2))
+unique(as.factor(ANO.sp$kartleggingsenhet_1m2))
+
+summary(ANO.sp)
+head(ANO.sp)
+
+#### data handling - reference data ####
