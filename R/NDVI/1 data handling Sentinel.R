4882136b043128a56494350f341d74a6d7fba6b8
joatop <joachim.topper@nina.no>
2023-05-15 14:35:46 +0200

    nin data handling continued

--- R/NDVI/1 data handling Sentinel.R
+++ R/NDVI/1 data handling Sentinel.R
@@ -12,25 +12,57 @@ library(sf)
 #############Import NiN data
 nin <- st_read("R:\\GeoSpatialData\\Habitats_biotopes\\Norway_Miljodirektoratet_Naturtyper_nin\\Original\\versjon20221231\\Natur_Naturtyper_nin_norge_med_svalbard_25833\\Natur_Naturtyper_NiN_norge_med_svalbard_25833.gdb")
 
+# fixing variable- and ecosystem-names with special characters
+colnames(nin)
 colnames(nin)[c(3,8,17,26,31,33,34)] <- c("hovedoekosystem","kartleggingsaar","noyaktighet",
                                        "omraadenavn","uk_naertruet","uk_sentraloekosystemfunksjon",
                                        "uk_spesieltdaarligkartlagt")
 unique(nin$hovedoekosystem)
-nin$hovedoekosystem <- as.factor(nin$hovedoekosystem)
-levels(nin$hovedoekosystem)
-levels(nin$hovedoekosystem)[c(3,4,6)] <- c("Vaatmark","Naturlig_aapne_lav","Naturlig_aapne_uskog")
 
-# checking mosaics, and main ecosystem type against mapping units
-nin[,c(3,15,24)]
-summary(as.factor(nin$mosaikk))
-# mosaic: data don't say proportion of NiN-types within mosaics -> omit mosaics
-# main types: it seems that main type and mapping unit often do not match -> use mapping units to inform separate main type variable
+nin <- nin %>% mutate(hovedoekosystem = recode(hovedoekosystem, 
+                                 "Våtmark" = 'Vaatmark',
+                                 "Semi-naturlig mark" = 'Semi_naturlig',
+                                 "Naturlig åpne områder i lavlandet" = 'Naturlig-aapne',
+                                 "Naturlig åpne områder under skoggrensa" = 'Naturlig_aapne')) %>%
+  mutate(validGeo = st_is_valid(SHAPE))
 
+# checking how many polygons have multiple ecosystem types
 unique(nin$ninkartleggingsenheter)
 
-unique(nin$ninkartleggingsenheter[nin$hovedoekosystem=="Naturlig_aapne_lav"])
-unique(nin$ninkartleggingsenheter[nin$hovedoekosystem=="Naturlig_aapne_uskog"])
-unique(nin$ninkartleggingsenheter[nin$hovedoekosystem=="Semi-naturlig"])
+nrow(nin)
+# 95469 polygons altogether
+nrow(nin %>%
+  filter(grepl(',', ninkartleggingsenheter))
+)
+# 21094 polygons have more than 1 ecosystem type (they are separated by commas in the ninkartleggingsenheter-variable)
+
+nrow(nin %>%
+       filter(!grepl(',', ninkartleggingsenheter))
+)
+# 74375 polygons should have only 1 ecosystem type
+
+# there's no information on the proportion of ecosystem types in the polygons, so we got to omit all polygons with multiple ecosystem types :(
+nin <- nin %>%
+  filter(!grepl(',', ninkartleggingsenheter))
+
+# fix the content in the ninkartleggingsenheter-variable
+summary(as.factor(nin$ninkartleggingsenheter))
+# get rid of the NA- in the beginning
+nin <- nin %>% mutate(ninkartleggingsenheter = str_remove(ninkartleggingsenheter, 'NA_'))
+# making a main ecosystem type variable
+nin <- nin %>% mutate(hovedtype = substr(ninkartleggingsenheter, 1, 3),
+               hovedtype = str_remove(hovedtype, '-'))
+# checking mapping unit against main ecosystem type
+nin[,c("hovedoekosystem","hovedtype")]
+summary(as.factor(nin$hovedtype[nin$hovedoekosystem=="Vaatmark"]))
+summary(as.factor(nin$hovedtype[nin$hovedoekosystem=="Semi-naturlig"]))
+summary(as.factor(nin$hovedtype[nin$hovedoekosystem=="Naturlig_aapne"]))
+# making a new variable for the overarching ecosystem types based on the main ecosystem types
+
+
+#### continue here ####
+
+
 
 
 
